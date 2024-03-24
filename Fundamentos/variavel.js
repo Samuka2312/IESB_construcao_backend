5cdbeddb44409be07dfa2b5b06e3afa4e0b034fa
Samuka2312 <contatosamuel.lima23@gmail.com>
2024-03-24 15:45:58 -0300

    todas alterações

---
+++ Fundamentos/variavel.js
@@ -0,0 +1,16 @@
+// Criando variável com "var"
+
+var fullname
+
+// Entre aspas de atribui a irformação à variável
+
+fullname = "Tiffany Raio Lazer"
+
+// console.log("Fala irmã", + fullname);
+
+var firstname = "Paula"
+var middlename = "Tejano"
+var lastname = "Pinto"
+var age = 22
+
+console.log(firstname, middlename, lastname, age);
